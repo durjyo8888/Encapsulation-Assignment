1c7db0892515b3775c5fd015362a28fa0b7228f3
Kata Durjyodhan Reddy <kdurjyodhan141@gmail.com>
2023-07-05 20:32:36 +0530

    Add files via upload
    git init

---
+++ encapsulation-durjyo8888/src/main/java/com/driver/Main.java
@@ -0,0 +1,9 @@
+package com.driver;
+
+public class Main {
+  public static void main(String[]args){
+      RWOnly obj = new RWOnly();
+      obj.setName("KD REDDY");
+      System.out.println(obj.getName());
+  }
+}
